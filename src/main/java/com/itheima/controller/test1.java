a9b036921e10175f0d2a0a03e5e4460b36d0b0e0
tianxu <40300534@qq.com>
2019-01-07 20:32:31 +0800

    4444

--- src/main/java/com/itheima/controller/test1.java
+++ src/main/java/com/itheima/controller/test1.java
@@ -14,7 +14,7 @@ public class test1 {
 
     @RequestMapping(value = "findAll")
     public String findAll(Model model) {
-        System.out.println("222");
+        System.out.println("2224444");
         return null;
     }
 }
