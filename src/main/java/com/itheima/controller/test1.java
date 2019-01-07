a4fe1a4c3a30dc49936a4120fe89adeb05cf2a37
tianxu <40300534@qq.com>
2019-01-07 19:39:39 +0800

    第一次提交1

---
+++ src/main/java/com/itheima/controller/test1.java
@@ -0,0 +1,19 @@
+package com.itheima.controller;
+
+import com.itheima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class test1 {
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(value = "findAll")
+    public String findAll(Model model) {
+        return null;
+    }
+}
